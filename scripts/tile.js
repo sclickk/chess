a04b8c035302eda27d46c7b16c45e2f6a809102f
Eric Villarreal <esvg.664252@gmail.com>
2018-12-19 16:37:28 -0600

    Extinguished the dumpster fire known as tile.js

--- scripts/tile.js
+++ scripts/tile.js
@@ -1,56 +1,48 @@
-function Tile(pos) {
-  this.pos = pos;
-  this.tileHTML = "<td class=\"tile\" id=\"" + this.pos + "\"></td>\n";
+class Tile {
+  constructor(pos) {
+    this.pos = pos;
+    this.tileHTML = "<td class=\"tile\" id=\"" + this.pos + "\"></td>\n";
+  }
+
+  /*
+   * Change the piece that's on a tile.
+   */
+  set(tile, piece) {
+    $('#' + tile).text(piece);
+  }
+
+  /*
+   * Returns the piece currently on a tile.
+   */
+  get(tile) {
+    return this.exists(tile) ? $('#' + tile).text() : "";
+  }
+
+  /*
+   * This function takes in a tile and determines the color of a piece on it.
+   * You ask "What player owns the piece on tile [a3, e4, etc.]?" and it
+   * returns "W", "B", or "E" (empty).
+   */
+  getPieceColor(tile) {
+    if (tile.match(/(♙|♖|♘|♗|♕|♔)/)) {
+      return "W";
+    } else if (tile.match(/(♟|♜|♞|♝|♛|♚)/)) {
+      return "B";
+    } else {
+      return "E";
+    }
+  }
+
   /*
    * Determine whether or not a given notation is valid for a
    * position on the board.
    */
-  this.exists = function (tile) {
+  exists(tile) {
     // Regular expressions let us match coordinate notation more easily.
     if (tile.length == 2 && tile.match(/[a-h][1-8]/)) {
       return true;
     } else {
       return false;
-    }
-  }
-  if (pos != undefined) {
-    if ($("#" + this.pos)[0]) {
-      console.log("hi");
-      $("#" + this.pos)[0].onclick = function () {
-        this.pos = pos;
-        $("#" + this.pos)[0].style = "border: 1px solid #f00;";
-        console.log("created border at " + this.pos);
-      }
-    }
-  } else {
-
-    /*
-     * Change the piece that's on a tile.
-     */
-    this.set = function (tile, piece) {
-      $('#' + tile).text(piece);
-    }
-
-    /*
-     * Returns the piece currently on a tile.
-     */
-    this.get = function (tile) {
-      return this.exists(tile) ? $('#' + tile).text() : "";
-    }
-
-    /*
-     * This function takes in a tile and determines the color of a piece on it.
-     * You ask "What player owns the piece on tile [a3, e4, etc.]?" and it
-     * returns "W", "B", or "E" (empty).
-     */
-    this.getPieceColor = function (tile) {
-      if (tile.match(/(♙|♖|♘|♗|♕|♔)/)) {
-        return "W";
-      } else if (tile.match(/(♟|♜|♞|♝|♛|♚)/)) {
-        return "B";
-      } else {
-        return "E";
-      }
     }
   }
 }
