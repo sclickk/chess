2c0b6cad14d1aed3d7fd60ffaca53a3049e1c60d
Eric Villarreal <esvg.664252@gmail.com>
2018-12-19 07:42:27 -0600

    Use query selectors

--- scripts/moves.js
+++ scripts/moves.js
@@ -2,26 +2,29 @@
  * This constructor manages the move log and keeps track of who's
  * turn it is to move.
  */
-function Moves() {
-  this.turn = "W";
-  this.turnHTML = document.getElementById("turnStatus");
-  this.movesList = document.getElementById("movesList");
+function Moves()
+{
+  this.turn = 'W';
+  this.turnStatus = $('#turnStatus');
+  this.movesList = $('#movesList');
   // These variables manage whether or not a player is checked.
   this.whiteChecked = false;
   this.blackChecked = false;
+
   /*
    * Switch who's turn it is to move.
    */
   this.switchTurn = function () {
-    this.turn = (this.turn == "W" ? "B" : "W");
-    this.turnHTML.innerHTML = (this.turn == "W" ? "White:" : "Black:");
+    this.turn = (this.turn == 'W' ? 'B' : 'W');
+    this.turnStatus.text(this.turn == 'W' ? 'White:' : 'Black:');
   }
+
   /*
    * Reset the turn to white at the start of the game.
    */
   this.resetTurn = function () {
-    this.turn = "W";
-    this.turnHTML.innerHTML = "White:";
+    this.turn = 'W';
+    this.turnStatus.text('White:');
   }
 
   /*
@@ -33,21 +36,21 @@ function Moves() {
    * 4. Kf1 ...
    */
   this.logMove = function (move) {
-    this.turn == "W" ?
-      this.movesList.innerHTML += "<li>" + move + "</li>":
-      this.movesList.lastChild.innerHTML += " " + move;
+    this.turn == 'W' ?
+      this.movesList.append('<li>' + move + '</li>'):
+      this.movesList.children().last().append(' ' + move);
   }
 
   this.logCheck = function(move) {
     // endsWith() is a built-in JavaScript function.
-    this.logMove(move + (move.endsWith("+") ? "" : "+"));
+    this.logMove(move + (move.endsWith('+') ? '' : '+'));
   }
 
   /*
    * Clears the move log.
    */
   this.clearMoveLog = function () {
-    this.movesList.innerHTML = "";
+    $('#movesList').text('');
   }
 
   /*
@@ -55,7 +58,7 @@ function Moves() {
    */
   this.moveError = function (text) {
     // alert() is a standard browser feature.
-    alert("Invalid move: " + text);
+    alert('Invalid move: ' + text);
   }
 
   this.verify = function (piece, move) {
