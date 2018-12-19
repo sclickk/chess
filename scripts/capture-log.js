f3f6835afb09a56f60520271eef08da5a937498e
Eric Villarreal <esvg.664252@gmail.com>
2018-12-19 08:00:26 -0600

    Capture Log: Better docs + query selectors + convery to class

--- scripts/capture-log.js
+++ scripts/capture-log.js
@@ -1,19 +1,28 @@
-function CaptureLog() {
-  /*
+class CaptureLog
+{
+  /**
+   * Create a log of what pieces have been captured in the game.
+   */
+  constructor()
+  {
+    this.whiteCaptured = $('#whiteCaptured');
+    this.blackCaptured = $('#blackCaptured');
+  }
+  /**
    * Clears the log of what pieces where captured at the start of the game.
    */
-  this.clear = function () {
-    $('#whiteCaptured').text("⚐: ");
-    $('#blackCaptured').text("⚑: ");
+  clear() {
+    this.whiteCaptured.text("⚐: ");
+    this.blackCaptured.text("⚑: ");
   }
-  /*
+  /**
    * Log a piece being captured by a player.
+   * @param turn the current player's turn
+   * @param piece the piece to be logged
    */
-  this.log = function (turn, piece) {
-    if (turn == "W") {
-      $('#whiteCaptured').append(piece);
-    } else {
-      $('#blackCaptured').append(piece);
-    }
+  log(turn, piece) {
+    turn == "W" ?
+      this.whiteCaptured.append(piece):
+      this.blackCaptured.append(piece);
   }
 }
