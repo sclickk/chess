909851622ef412acc830185c093d3fd8754c7a3f
Eric Villarreal <esvg.664252@gmail.com>
2018-12-20 00:02:47 -0600

    Update queen and rook

--- scripts/pieces/queen.js
+++ scripts/pieces/queen.js
@@ -16,11 +16,21 @@ Queen.prototype.getRangeOfMovement = function () {
   var range = new Array();
   var tiles = new Array();
   var oppositeColor = (this.color == "W" ? "B" : "W");
-  // Add every piece to the left of the queen.
+  // Add every tile to the left of the queen.
   for (var i = toNumber(this.pos[0]); i >= 0; i--) {
-    var c = toLetter(0 - -i) + this.pos[1];
+    var c = Tile.fromRef(this.pos, i, 0);
+    // Make sure the tile actually exists.
     if (Tile.exists(c)) {
+      // Don't add the current position of the rook.
       if (c != this.pos) {
+        // When it evaluates the next tile, one of the following
+        // should happen:
+        // * If the tile is empty, just add the tile and evaluate the next.
+        // * If the tile has a piece:
+        //   * If it's color is the opposite of this piece:
+        //     * Add the tile and break the loop.
+        //   * If it's color is the same of this piece:
+        //     * Break the loop.
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
           range = range.concat(c);
           i = -100;
@@ -34,9 +44,9 @@ Queen.prototype.getRangeOfMovement = function () {
       }
     }
   }
-  // Add every piece to the right of the queen.
+  // Add every tile to the right of the queen.
   for (var i = toNumber(this.pos[0]); i <= 8; i++) {
-    var c = toLetter(0 - -i) + this.pos[1];
+    var c = Tile.fromRef(this.pos, i, 0);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
@@ -52,9 +62,9 @@ Queen.prototype.getRangeOfMovement = function () {
       }
     }
   }
-  // Add every piece below the queen.
+  // Add every tile below the queen.
   for (var i = this.pos[1]; i >= 0; i--) {
-    var c = this.pos[0] + (0 - -i);
+    var c = Tile.fromRef(this.pos, 0, i);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
@@ -70,9 +80,9 @@ Queen.prototype.getRangeOfMovement = function () {
       }
     }
   }
-  // Add every piece to the up of the queen.
+  // Add every tile above the queen.
   for (var i = this.pos[1]; i <= 8; i++) {
-    var c = this.pos[0] + (0 - -i);
+    var c = Tile.fromRef(this.pos, 0, i);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
--- scripts/pieces/rook.js
+++ scripts/pieces/rook.js
@@ -14,7 +14,7 @@ Rook.prototype.getRangeOfMovement = function () {
   var oppositeColor = (this.color == "W" ? "B" : "W");
   // Add every tile to the left of the rook.
   for (var i = toNumber(this.pos[0]); i >= 0; i--) {
-    var c = toLetter(0 - -i) + this.pos[1];
+    var c = Tile.fromRef(this.pos, i, 0);
     // Make sure the tile actually exists.
     if (Tile.exists(c)) {
       // Don't add the current position of the rook.
@@ -42,7 +42,7 @@ Rook.prototype.getRangeOfMovement = function () {
   }
   // Add every tile to the right of the rook.
   for (var i = toNumber(this.pos[0]); i <= 8; i++) {
-    var c = toLetter(0 - -i) + this.pos[1];
+    var c = Tile.fromRef(this.pos, i, 0);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
@@ -60,7 +60,7 @@ Rook.prototype.getRangeOfMovement = function () {
   }
   // Add every tile below the rook.
   for (var i = this.pos[1]; i >= 0; i--) {
-    var c = this.pos[0] + (0 - -i);
+    var c = Tile.fromRef(this.pos, 0, i);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
@@ -78,7 +78,7 @@ Rook.prototype.getRangeOfMovement = function () {
   }
   // Add every tile above the rook.
   for (var i = this.pos[1]; i <= 8; i++) {
-    var c = this.pos[0] + (0 - -i);
+    var c = Tile.fromRef(this.pos, 0, i);
     if (Tile.exists(c)) {
       if (c != this.pos) {
         if (Tile.getPieceColor(Tile.get(c)) == oppositeColor) {
